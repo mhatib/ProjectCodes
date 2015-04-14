de17a4704dda83bdd1a882f7c636123b2a81b590
mhatib <muhd.hatib@gmail.com>
2015-04-14 22:35:17 +0800

    First commit

---
+++ src/Client1.java
@@ -0,0 +1,39 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.Socket;
+
+
+public class Client1 {
+	public static void main(String[] args) throws Exception {
+        String hostName = "localhost";        
+        int portNumber = 4321;
+        Socket socket = new Socket(hostName, portNumber);        
+        
+        File file = new File("src//disp.pdf");
+        double length = file.length();
+        if (length > Integer.MAX_VALUE) {
+            System.out.println("File is too large.");
+        }
+        byte[] bytes = new byte[(int) length];
+        
+        socket.setSoTimeout(10000);
+        FileInputStream fileIn = new FileInputStream(file);
+        BufferedInputStream buffIn = new BufferedInputStream(fileIn);
+        BufferedOutputStream buffOut = new BufferedOutputStream(socket.getOutputStream());
+        
+        int count;
+        while ((count = buffIn.read(bytes)) > 0) {
+        	buffOut.write(bytes, 0, count);
+        }
+
+        fileIn.close();
+        buffIn.close();
+        socket.close();
+        
+         	
+    	  
+    }
+}
+
