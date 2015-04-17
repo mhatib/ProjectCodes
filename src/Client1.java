7ac1d06d3fcac3764b088c2588bc527f511576b4
mhatib <muhd.hatib@gmail.com>
2015-04-17 10:32:27 +0800

    - encryption and decryption of message

--- src/Client1.java
+++ src/Client1.java
@@ -1,46 +1,73 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.security.Key;
-import java.security.KeyPair;
-import java.security.KeyStore;
-import java.security.PrivateKey;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import java.security.cert.Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 
 public class Client1 {
 	public static void main(String[] args) throws Exception {
+		InputStream is = null;
+		InputStreamReader isr = null;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        BufferedReader br = null;
+		
         String hostName = "localhost";        
         int portNumber = 4321;
-        Socket socket = new Socket(hostName, portNumber);        
+        Socket socket = new Socket(hostName, portNumber);   
         
-        File file = new File("src//disp.pdf");
+        PrintWriter pout = new PrintWriter(socket.getOutputStream(), true);		
+		pout.println("Hello SecStore, please prove your identity");
+		String initR = "Hello, this is SecStore";
+		boolean trust = false;
+		
+		
+		DataInputStream dIn = new DataInputStream(socket.getInputStream());
+		int length = dIn.readInt();                    // read length of incoming message
+		if(length>0) {
+		    byte[] message = new byte[length];
+		    dIn.readFully(message, 0, message.length); // read the message
+		    PublicKey publicKey = readPublicKey("src//publicServer.der");
+	        byte[] recovered_message = decrypt(publicKey, message);
+	        String received = new String(recovered_message, "UTF8");
+	        if (received.equals(initR)){
+	        	trust = true;
+	        }
+		}
+		
+		if (trust){
+			System.out.println("Let's go");
+		}
+        
+        
+        
+/*        File file = new File("src//disp.pdf");
         double length = file.length();
         if (length > Integer.MAX_VALUE) {
             System.out.println("File is too large. Final test");
         }
         byte[] bytes = new byte[(int) length];
         
-        KeyStore ks = KeyStore.getInstance("PKCS12");
-        FileInputStream fis = new FileInputStream("src//publicServer.der");
-        ks.load(fis, "password".toCharArray()); // There are other ways to read the password.
-        fis.close();
-        String alias = "myalias";
-
-        Key key = ks.getKey(alias, "password".toCharArray());
-        if (key instanceof PrivateKey) {
-          // Get certificate of public key
-          Certificate cert = ks.getCertificate(alias);
-
-          // Get public key
-          PublicKey publicKey = cert.getPublicKey();
-
-          // Return a key pair
-          new KeyPair(publicKey, (PrivateKey) key);
-        }
+       
         socket.setSoTimeout(10000);
         FileInputStream fileIn = new FileInputStream(file);
         BufferedInputStream buffIn = new BufferedInputStream(fileIn);
@@ -53,10 +80,30 @@ public class Client1 {
 
         fileIn.close();
         buffIn.close();
-        socket.close();
+        socket.close();*/
         
          	
     	  
     }
+	
+	public static byte[] decrypt(PublicKey key, byte[] ciphertext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+	{
+	    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");   
+	    cipher.init(Cipher.DECRYPT_MODE, key);  
+	    return cipher.doFinal(ciphertext);
+	}
+	
+	public static byte[] readFileBytes(String filename) throws IOException
+	{
+	    Path path = Paths.get(filename);
+	    return Files.readAllBytes(path);        
+	}
+
+	public static PublicKey readPublicKey(String filename) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
+	{
+	    X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(readFileBytes(filename));
+	    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+	    return keyFactory.generatePublic(publicSpec);       
+	}
 }
 
--- src/Server1.java
+++ src/Server1.java
@@ -1,27 +1,87 @@
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 
 public class Server1 {
 	public static void main(String[] args) throws Exception {
+		InputStream is = null;
+		InputStreamReader isr = null;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        BufferedReader br = null;
+        		
+		//pout.println("Hello SecStore, please prove your identity");
+        
 		ServerSocket serverSocket = new ServerSocket(4321);
     	System.out.println("(... expecting connection ...)");
         Socket socket = serverSocket.accept();     
     	System.out.println("(... connection established ...)");
-        InputStream is = null;
-        FileOutputStream fos = null;
-        BufferedOutputStream bos = null;
-        int bufferSize = 0;
+    	
+    	
+    	String init = "Hello SecStore, please prove your identity";
+    	String initR = "Hello, this is SecStore";
+    	
+    	is = socket.getInputStream();
+    	isr = new InputStreamReader(is);
+        br = new BufferedReader(isr);
+        String text = br.readLine();
+    	if (text.equals(init)){
+    		PrivateKey privateKey = getPrivateKey("src//privateServer.der");
+        	byte[] message = initR.getBytes("UTF8");
+        	byte[] secret = encrypt(privateKey, message);
+        	DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+        	dOut.writeInt(secret.length); 
+        	dOut.write(secret);
+        	
+
+    	}
+    	
+    	
+    	PublicKey publicKey = readPublicKey("src//publicServer.der");
+        
+        
+        
+        //byte[] recovered_message = decrypt(publicKey, secret);
+        //System.out.println(new String(recovered_message, "UTF8"));
+        
+    	
+        
+
+    	
+        /*int bufferSize = 0;
         is = socket.getInputStream();
 
         bufferSize = socket.getReceiveBufferSize();
         System.out.println("Buffer size: " + bufferSize);
         
         //TODO: add same file type as client. Perhaps get file name and parse for file type?
-        fos = new FileOutputStream("src//disp2.pdf");
+        fos = new FileOutputStream("src//disp3.pdf");
         bos = new BufferedOutputStream(fos);
         byte[] bytes = new byte[bufferSize];
 
@@ -35,7 +95,48 @@ public class Server1 {
         bos.close();
         is.close();
         socket.close();
-        serverSocket.close();
+        serverSocket.close();*/
+	}
+	
+	public static PrivateKey getPrivateKey(String filename) throws Exception {
+
+        File f = new File(filename);
+        FileInputStream fis = new FileInputStream(f);
+        DataInputStream dis = new DataInputStream(fis);
+        byte[] keyBytes = new byte[(int) f.length()];
+        dis.readFully(keyBytes);
+        dis.close();
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePrivate(spec);
+    }
+	
+	public static byte[] encrypt(PrivateKey key, byte[] plaintext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+	{
+	    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");   
+	    cipher.init(Cipher.ENCRYPT_MODE, key);  
+	    return cipher.doFinal(plaintext);
+	}
+	
+	public static byte[] decrypt(PublicKey key, byte[] ciphertext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+	{
+	    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");   
+	    cipher.init(Cipher.DECRYPT_MODE, key);  
+	    return cipher.doFinal(ciphertext);
+	}
+	
+	public static byte[] readFileBytes(String filename) throws IOException
+	{
+	    Path path = Paths.get(filename);
+	    return Files.readAllBytes(path);        
+	}
+
+	public static PublicKey readPublicKey(String filename) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException
+	{
+	    X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(readFileBytes(filename));
+	    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+	    return keyFactory.generatePublic(publicSpec);       
 	}
 }
 
