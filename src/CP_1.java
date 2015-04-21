cdbef9e4e30163e9974f556d008b508e3b754939
mhatib <muhd.hatib@gmail.com>
2015-04-22 01:12:00 +0800

    - final test

--- src/CP_1.java
+++ src/CP_1.java
@@ -1,5 +1,6 @@
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.security.Key;
@@ -17,19 +18,56 @@ public class CP_1 {
 		    int portNumber = 4321;
 		    Socket socket = new Socket(hostName, portNumber);
 		    
+		  //Write nonce to server
+		    String nonce = "Hello SecStore, please prove your identity";
 		    PrintWriter pout = new PrintWriter(socket.getOutputStream(), true);		
-			pout.println("Hello SecStore, please prove your identity");
-			message = UploaderHelper.receiveByteArray(socket);
+			pout.println(nonce);
+			
+			//Receive encrypted nonceMsg from server
+			byte[] nonceMsg = UploaderHelper.receiveByteArray(socket);
+			
 			pout.println("Send me your certificate signed by CA");
 			
 			byte[] cert = UploaderHelper.receiveByteArray(socket);
 			UploaderHelper.saveBytes("cserve.crt",cert);
 			
 	    	FileInputStream inStream = new FileInputStream("src//cserve.crt");
-	        X509Certificate CAcert = X509Certificate.getInstance(inStream);
+	        X509Certificate serverCert = X509Certificate.getInstance(inStream);
+	        PublicKey serverKey = serverCert.getPublicKey();
+	        
+	      //Authentication Protocol begins here
+	        
+	        //Create X509Certificate object from CA.crt
+	        InputStream inStream2 = new FileInputStream("src//CA.crt");
+	        X509Certificate CAcert = X509Certificate.getInstance(inStream2);
+	        
+	        //Extract Public key of the CA from the CA.crt.
 	        PublicKey CAKey = CAcert.getPublicKey();
 	        
-	        byte[] dec = UploaderHelper.decrypt(CAKey,message);
+	        //Check the validity and verify signed certificate.
+	        try{
+	        	System.out.println("Checking CA's validity...");
+	        	CAcert.checkValidity();
+	        }catch(Exception e){
+	        	System.out.println(e);
+	        	System.out.println("CSE-CA Certificate is invalid!");
+	        }
+	        try{
+	        	System.out.println("Checking the validity of server's certificate...");
+	        	serverCert.checkValidity();
+	        }catch(Exception e){
+	        	System.out.println(e);
+	        	System.out.println("Server's certificate is invalid!");
+	        }
+	        try{
+	        	System.out.println("Verifying CSE-CA and server's certificates");
+	        	serverCert.verify(CAKey);
+	        }catch(Exception e){
+	        	System.out.println(e);
+	        	System.out.println("CSE-CA and Server's certificate failed in verification!");
+	        }
+	        
+	        byte[] dec = UploaderHelper.decrypt(serverKey,nonceMsg);
 	        
 			System.out.println(new String(dec,"UTF8"));
 			
@@ -50,7 +88,7 @@ public class CP_1 {
 //			System.out.println("Start encryption");
 			for(int i=0; i<encFile.length; i++){
 				//System.out.println("Encrypting "+i+" block");
-				byte[] a=UploaderHelper.encryptPub(CAKey, encFile[i]);
+				byte[] a=UploaderHelper.encryptPub(serverKey, encFile[i]);
 				UploaderHelper.sendBytes(a, socket);
 				//fileOut.writeInt(a.length);
 				//fileOut.write(a);
--- src/CP_2.java
+++ src/CP_2.java
@@ -1,8 +1,8 @@
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.security.Key;
-import java.security.MessageDigest;
 import java.security.PublicKey;
 
 import javax.crypto.Cipher;
@@ -40,6 +40,40 @@ public class CP_2 {
     	FileInputStream inStream = new FileInputStream("src//cserve.crt");
         X509Certificate serverCert = X509Certificate.getInstance(inStream);
         PublicKey serverKey = serverCert.getPublicKey();
+        
+//Authentication Protocol begins here
+        
+        //Create X509Certificate object from CA.crt
+        InputStream inStream2 = new FileInputStream("src//CA.crt");
+        X509Certificate CAcert = X509Certificate.getInstance(inStream2);
+
+       
+        
+        //Extract Public key of the CA from the CA.crt.
+        PublicKey CAKey = CAcert.getPublicKey();
+        
+        //Check the validity and verify signed certificate.
+        try{
+        	System.out.println("Checking CA's validity...");
+        	CAcert.checkValidity();
+        }catch(Exception e){
+        	System.out.println(e);
+        	System.out.println("CSE-CA Certificate is invalid!");
+        }
+        try{
+        	System.out.println("Checking the validity of server's certificate...");
+        	serverCert.checkValidity();
+        }catch(Exception e){
+        	System.out.println(e);
+        	System.out.println("Server's certificate is invalid!");
+        }
+        try{
+        	System.out.println("Verifying CSE-CA and server's certificates");
+        	serverCert.verify(CAKey);
+        }catch(Exception e){
+        	System.out.println(e);
+        	System.out.println("CSE-CA and Server's certificate failed in verification!");
+        }
         
         //Decrypt nonceMsg
         byte[] dec = UploaderHelper.decrypt(serverKey,nonceMsg);
